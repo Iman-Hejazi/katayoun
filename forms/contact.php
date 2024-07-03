3ceabb15119728325b57cba685888822608b9c05
Iman Hejazi <hejazi.iman@gmail.com>
2024-07-03 11:16:01 -0600

    Add email address

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,7 @@
 <?php
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'khojasteh.katayoon@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -11,7 +11,7 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
